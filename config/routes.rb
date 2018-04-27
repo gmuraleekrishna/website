cbe35bbafa5b992b78cf4844a98ea0b4cb15175e
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-04-28 01:01:19 +1000

    Add payment link

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: 'home#index'
   end
+
+  get '/pay', to: redirect('https://paypal.me/gmuraleekrishna')
 end
